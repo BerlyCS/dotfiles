449946c4b086a9748b9516803673d9f0b821044e
BerlyCS <bduenasm@unsa.edu.pe>
2025-01-05 10:44:00 -0500

    extract

--- extract.sh
+++ extract.sh
@@ -9,3 +9,8 @@ cp -r ~/.config/nvim/plug-config/ .config/nvim
 cp ~/.tmux.conf .
 
 cp ~/.config/fish/config.fish .config/fish
+
+cp ~/.config/waybar/config .config/waybar
+cp ~/.config/waybar/style.css .config/waybar
+
+cp ~/.config/sway/config .config/sway
