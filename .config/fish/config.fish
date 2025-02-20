737aba62a8dd18731bb32d33d2a55002561c2fe8
BerlyCS <bduenasm@unsa.edu.pe>
2025-02-20 09:40:18 -0500

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.scripts/lscolors.csh
     export tide_pwd_bg_color=285940
+    export tide_git_icon=\ue702
     export PATH="$PATH:/home/lenovo/.scripts/bin:/home/lenovo/bin"
 end
